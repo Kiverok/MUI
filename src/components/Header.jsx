19da7ff8b00ac79f9cda876ce762737bb6e53912
Kiverok <kiver566@ukr.net>
2023-02-03 21:59:12 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { ShoppingBasket } from "@mui/icons-material"
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
+
+
+export const Header = () => {
+  return (
+    <AppBar position="static">
+        <Toolbar>
+<Typography
+variant="h6"
+component="span"
+sx={{ flexGrow: 1 }}
+>
+    MUI Shop 
+</Typography>
+<IconButton
+color="inherit"
+>
+    <ShoppingBasket />
+</IconButton>
+        </Toolbar>
+    </AppBar>
+  )
+}
